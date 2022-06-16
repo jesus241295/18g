9807cb5bc7247fcba5e2d1b11d25b498e217597d
Jesus Gonzalez <jesusgonvale@gmail.com>
2022-06-16 16:39:28 -0500

    se crea un switch con React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 // components
@@ -8,18 +8,40 @@ import FullName from "./components/FullName";
 function App() {
   // estado local del componente
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  // useEffect(() => {
+  //   console.log("USE_EFFECT");
+  // }, []);
+  // useEffect(() => {
+  //   console.log("ESE_EFFECT");
+  // });
 
+  const handleClick = (increase) => {
+    let newCount = count;
+    if (increase) {
+      newCount = newCount + 1;
+    } else {
+      newCount = newCount - 1;
+    }
+    setCount(newCount);
+  };
+  const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
+  const counType = isMultiple ? "Es multiplo" : "no es multiple";
+  const countClass = isMultiple ? "active" : "inactive";
   return (
     <div className="App">
       <header className="App-header">
         <Title text="chucho"></Title>
-
         <FullName fisrtName="Jesus" lastName="Gonzalez"></FullName>
-
-        <p>Counter {count}</p>
-        <button onClick={() => setCount(count + 1)}>Incremento</button>
-
-        <button onClick={() => setCount(count - 1)}>Decremento</button>
+        <p className={countClass}>
+          Counter: {count}, {counType}
+        </p>
+        <button onClick={() => handleClick(true)}>Increase</button>
+        <div className={`square ${isOn ? "square-on" : "square-off"} `}>
+          {isOn ? "ON" : "OFF"}
+        </div>
+        <button onClick={() => setIsOn(true)}>On</button>
+        <button onClick={() => setIsOn(false)}>OFF</button>
       </header>
     </div>
   );
